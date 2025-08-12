149ed863f1bc6f0a2cfcc0062ce0bdaedcadeccc
ocoodo <enjinix@gmail.com>
2025-08-12 14:44:01 +0400

    feat(middleware):Added cors middleware

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.database import create_tables
-
 
 app = FastAPI(
     title="SecretLink API"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# Include 
+# Include routers
 from src.links.router import router as link_router
 app.include_router(link_router)
 
